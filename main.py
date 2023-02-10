26d154aa8e482a81be55c1722aea509bb72ed219
Dayvid <dayvid7santos2002@outlook.com>
2023-02-10 04:18:39 -0300

    added last validation with datetime

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import mysql.connector
 from flask import Flask, jsonify, make_response, request
 from datetime import datetime
+import calendar
+
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -33,7 +35,7 @@ def retrieve_all_credit_cards():  #this function will take all credit cards from
 
     return make_response(
         jsonify(
-            message= 'All credit cards!',
+            message= 'Here are all credit cards!',
             data= credit_cards_formated
         )
     )
@@ -41,7 +43,7 @@ def retrieve_all_credit_cards():  #this function will take all credit cards from
 
 
 @app.route('/credit-cards',methods = ['POST'])
-def create_credit_card():  
+def create_credit_card():    #function are much big. must be refatored soon.
     my_cursor = sql_connection.cursor() 
     credit_card= request.json
 
@@ -56,10 +58,22 @@ def create_credit_card():
         return make_response(jsonify( message='invalid date, try YYYY-MM-DD'))
     if datetime.now() > datetime.strptime(exp_date,'%Y-%m-%d'):
         return make_response(jsonify( message="Your card expedition date cannot be less than today's date."))
-
-
-    command= f'insert into creditCards (exp_date, holder, number, cvv, brand)VALUES ("{exp_date}", "{holder}",{number}, {cvv}, "{brand}")'
     
+    date_object = datetime.strptime(exp_date, '%Y-%m-%d').date() #This is to transform our exp_date at format datetime
+    year=date_object.year #This just is possible, cuz date_object is in DateTime format. 
+    month = date_object.month
+    
+    last_day_of_the_month= calendar.monthrange(year,month )[1] #This position of array returns how much days the month has
+    exp_date = date_object.replace(day=last_day_of_the_month)
+
+
+    
+    
+    
+
+    #After going through all validations, now it's time to send the credit card to database.
+    command= f'insert into creditCards (exp_date, holder, number, cvv, brand)VALUES ("{exp_date}", "{holder}",{number}, {cvv}, "{brand}")'
+
     my_cursor.execute(command)
     sql_connection.commit()
 
