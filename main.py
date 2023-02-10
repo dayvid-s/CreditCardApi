204c86a34ec1148816ad4431e40200891eca8724
Dayvid <dayvid7santos2002@outlook.com>
2023-02-09 21:12:58 -0300

    added create_credit_card method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -11,18 +11,19 @@ sql_connection = mysql.connector.connect(
     password='083a609da@',
     database='credit-card-bd'   
 )
-my_cursor = sql_connection.cursor()   # all sql commands must be inside of this guy, and before the close function
 
+my_cursor = sql_connection.cursor() # all sql commands must be inside of this guy, and before the close function
 
 @app.route('/credit-cards',) # req of type get by default.
 def retrieve_all_credit_cards():  #this function will take all credit cards from database, and return to api rest
+    my_cursor = sql_connection.cursor() 
     my_cursor.execute('SELECT * FROM creditCards')
     credit_cards = my_cursor.fetchall()
     credit_cards_formated = list()
 
     for card in credit_cards:
         credit_cards_formated.append({
-            'id_credit_card':card[0],
+            'idcreditcards': card[0],
             'exp_date': card[1],
             'holder':card[2],
             'number':card[3],
@@ -34,6 +35,31 @@ def retrieve_all_credit_cards():  #this function will take all credit cards from
         jsonify(
             message= 'All credit cards!',
             data= credit_cards_formated
+        )
+    )
+
+
+
+@app.route('/credit-cards',methods = ['POST'])
+def create_credit_card():  
+    my_cursor = sql_connection.cursor() 
+    credit_card= request.json
+
+    exp_date= credit_card['exp_date']
+    holder= credit_card['holder']
+    number= credit_card['number']
+    cvv= credit_card['cvv']
+    brand= credit_card['brand']
+
+    command= f'insert into creditCards (exp_date, holder, number, cvv, brand)VALUES ("{exp_date}", "{holder}",{number}, {cvv}, "{brand}")'
+
+    my_cursor.execute(command)
+    sql_connection.commit()
+
+    return make_response(
+        jsonify(
+            message= 'Credit card registered with successful!',
+            card = credit_card
         )
     )
 
